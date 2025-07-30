c0148a7b99f7b9be40f5e1be6b943d6b1f5d78c9
jeakaey <tiayokarelle@gmail.com>
2025-07-31 00:10:11 +0200

    deployment phase

--- src/apolloClient.js
+++ src/apolloClient.js
@@ -1,8 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: 'https://fittrack-server-fmbz.onrender.com/graphql',
+  uri: process.env.REACT_APP_GRAPHQL_URI,
   cache: new InMemoryCache()
 });
+
 
 export default client;
